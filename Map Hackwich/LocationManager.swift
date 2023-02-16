93a6598eb9c58a3c41b3f972d69783a8c4f377c5
Owen Johnson <owenbjohnson10@gmail.com>
2023-02-16 09:25:33 -0600

    Added User Location

---
+++ Map Hackwich/LocationManager.swift
@@ -0,0 +1,20 @@
+//
+//  LocationManager.swift
+//  Map Hackwich
+//
+//  Created by Owen Johnson on 2/8/23.
+//
+
+import Foundation
+import CoreLocation
+
+class LocationManager: NSObject, CLLocationManagerDelegate, ObservableObject {
+    var locationManager = CLLocationManager()
+    
+    override init() {
+           super.init()
+           locationManager.delegate = self
+           locationManager.requestWhenInUseAuthorization()
+           locationManager.startUpdatingLocation()
+       }
+}
