e1c0931f691c17f91a4843534f62cdde04a7ddb2
Owen Johnson <owenbjohnson10@gmail.com>
2023-02-16 09:30:37 -0600

    Added a location to the map

--- Map Hackwich/ContentView.swift
+++ Map Hackwich/ContentView.swift
@@ -9,6 +9,7 @@ import SwiftUI
 import MapKit
 
 struct ContentView: View {
+   
     @StateObject var locationManager = LocationManager()
     @State private var userTrackingMode: MapUserTrackingMode = .follow
     @State private var region = MKCoordinateRegion(
@@ -19,13 +20,46 @@ struct ContentView: View {
             latitudeDelta: 0.05,
             longitudeDelta: 0.05)
     )
+    @State private var places = [Place(name: "Barrington High School",
+
+                           coordinate: CLLocationCoordinate2D(
+
+                               latitude: 42.15704, longitude: -88.14812))]
+
     var body: some View {
-        Map(
-            coordinateRegion: $region,
+        Map(coordinateRegion: $region,
             interactionModes: .all,
             showsUserLocation: true,
-            userTrackingMode: $userTrackingMode
-        )
+            userTrackingMode: $userTrackingMode,
+            annotationItems: places) { place in
+            MapAnnotation(coordinate: place.coordinate,
+                          anchorPoint: CGPoint(x: 0.5, y: 1.2)) {
+                Marker(name: place.name)
+            }
+        }
+    }
+}
+struct Place: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+struct Marker: View {
+    var name: String
+    var body: some View {
+        ZStack {
+            VStack {
+                Spacer(minLength: 15)
+                Rectangle()
+                    .fill(Color.black)
+                    .frame(width: 30, height: 30, alignment: .center)
+                    .rotationEffect(.degrees(45))
+            }
+            Capsule()
+                .fill(Color.red)
+                .frame(width: 200, height: 30, alignment: .center)
+            Text(name)
+        }
     }
 }
 
